9a0e9becbd15c1437b9eb774063efc20d360cb55
Alexander Lairich <alexanderlairich@gmx.net>
2023-09-06 13:30:04 +0200

    Basic ball is finished

--- Bouncy Ball/Assets/Content/Ball/BallController.cs
+++ Bouncy Ball/Assets/Content/Ball/BallController.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    new Rigidbody2D rigidbody;
+    public float ballSpeed = 100.0f;
+    new private Rigidbody2D rigidbody;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void Start()
     {
-        if (rigidbody.velocity == Vector2.zero) 
-            rigidbody.AddForce(Vector2.up * 850);
+        rigidbody.velocity = Vector2.up * ballSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Return when not ball
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        // Calculate hit Factor
+        float x = HitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+
+        // Calculate direction, set length to 1
+        Vector2 direction = new Vector2(x, 1).normalized;
+
+        // Set velocity with direction * speed
+        rigidbody.velocity = direction * ballSpeed;
+
+        transform.localScale = new(direction.x, direction.y);
+    }
+
+    private float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x) / racketWidth;
     }
 }
